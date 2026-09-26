ea07fad94d111a65604ba3f83d4c77f57b5c4ee0
Soumil <131894442+soumiil@users.noreply.github.com>
2026-09-26 15:46:04 +0530

    Refactor main method to comment out employee creation examples and streamline employee lists initialization

--- src/Main.java
+++ src/Main.java
@@ -2,42 +2,47 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        
+        // // FULLTIME EMPLOYEES
+        // FullTime obj = new FullTime(101, "aditya", map);
+        // obj.setSalary(5000);
+        // System.out.println("Your salary is : " + obj.getSalary());
+        // obj.getSalarySlip();
+        
+        // // PARTTIME EMPLOYEES
+        // PartTime pt1 = new PartTime("shiva", 2000);
+        // pt1.setSalary(2000);
+        // System.out.println("your salary is : " + pt1.getSalary());
+        // pt1.getSalarySlip();
+        
+        // PartTime pt2 = new PartTime("SOUMIL", 1000);
+        // pt2.setSalary(2000);
+        // System.out.println("your salary is : " + pt2.getSalary());
+        // pt2.getSalarySlip();
+        
+        // PartTime pt3 = new PartTime("PALAK", 100);
+        // pt3.setSalary(2000);
+        // System.out.println("your salary is : " + pt3.getSalary());
+        // pt3.getSalarySlip();
+        
+        // List<PartTime> partTimeEmployees = new ArrayList<>(List.of(pt1, pt2, pt3));
+        // System.out.println("PartTime Employees: " + partTimeEmployees);
+        
+        // // CONTRACT EMPLOYEES
+        // Contract obj4 = new Contract("aditya", 100);
+        // obj4.setSalary(2000);
+        // System.out.println("your salary is : " + obj4.getSalary());
+        // obj4.getSalarySlip();
+        
+        // System.out.println(map);
+        
         HashMap<Integer, FullTime> map = new HashMap<>();
-
-        // FULLTIME EMPLOYEES
-        FullTime obj = new FullTime(101, "aditya", map);
-        obj.setSalary(5000);
-        System.out.println("Your salary is : " + obj.getSalary());
-        obj.getSalarySlip();
-
-        // PARTTIME EMPLOYEES
-        PartTime pt1 = new PartTime("shiva", 2000);
-        pt1.setSalary(2000);
-        System.out.println("your salary is : " + pt1.getSalary());
-        pt1.getSalarySlip();
-
-        PartTime pt2 = new PartTime("SOUMIL", 1000);
-        pt2.setSalary(2000);
-        System.out.println("your salary is : " + pt2.getSalary());
-        pt2.getSalarySlip();
-
-        PartTime pt3 = new PartTime("PALAK", 100);
-        pt3.setSalary(2000);
-        System.out.println("your salary is : " + pt3.getSalary());
-        pt3.getSalarySlip();
-
-        List<PartTime> partTimeEmployees = new ArrayList<>(List.of(pt1, pt2, pt3));
-        System.out.println("PartTime Employees: " + partTimeEmployees);
-
-        // CONTRACT EMPLOYEES
-        Contract obj4 = new Contract("aditya", 100);
-        obj4.setSalary(2000);
-        System.out.println("your salary is : " + obj4.getSalary());
-        obj4.getSalarySlip();
-
-        System.out.println(map);
+        List<FullTime> ftEmployees = new ArrayList<>();
+        List<PartTime> ptEmployees = new ArrayList<>();
+        List<Contract> ctEmployees = new ArrayList<>();
 
         Scanner sc = new Scanner(System.in);
+
         while (true) {
             System.out.print(
                     "Create an Employee\n1 - FullTime Employee\n2 - PartTime Employee\n3 - Contract Employee\n4 - Exit\nEnter your choice: ");
@@ -45,9 +50,6 @@ public class Main {
             sc.nextLine();
             int id;
             String name;
-            List<FullTime> ftEmployees = new ArrayList<>();
-            List<PartTime> ptEmployees = new ArrayList<>();
-            List<Contract> ctEmployees = new ArrayList<>();
 
             switch (n) {
                 case 1:
